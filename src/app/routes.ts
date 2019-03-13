0e0b7ac5a005f2cec487913ce82cde23ef3c0e03
Calin Leordean <CLeordean@grt.granitenet.com>
2019-03-13 11:18:05 -0400

    Protecting multiple routes with a single guard route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,10 +5,29 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
+// protecting one single route with Route Guard - below
+
+// export const appRoutes: Routes = [
+//     { path: 'home', component: HomeComponent },
+//     { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+//     { path: 'messages', component: MessagesComponent },
+//     { path: 'lists', component: ListsComponent },
+//     { path: '**', redirectTo: 'home', pathMatch: 'full'},
+// ];
+
+// protecting multiple routes with Route Guard - below
+
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
-    { path: 'messages', component: MessagesComponent },
-    { path: 'lists', component: ListsComponent },
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '', component: HomeComponent },
+    {
+        path: '',   // http://lolahost:4200/   - '' its empty because concatenates the below paths
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent },
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
