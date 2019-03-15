1598779c458793e2d52ae23de192a9f750d91296
Calin Leordean <CLeordean@grt.granitenet.com>
2019-03-15 11:00:28 -0400

    Ceated Member Detailed View Component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,6 +4,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 // protecting one single route with Route Guard - below
 
@@ -25,6 +26,7 @@ export const appRoutes: Routes = [
         canActivate: [AuthGuard],
         children: [
             { path: 'members', component: MemberListComponent },
+            { path: 'members/:id', component: MemberDetailComponent },
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent },
         ]
